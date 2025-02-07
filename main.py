24f582969a1e0ba894a1035b65a992bae0c2f0a0
yuproper <yuproper@gmail.com>
2025-02-07 21:33:38 +0700

    endpoint develop
    endpoint develop

--- main.py
+++ main.py
@@ -7,9 +7,8 @@ import time
 # Инициализация Pygame
 pygame.init()
 
-# Цвета
-WHITE = (255, 255, 255)
-GREEN = (0, 255, 0)
+
+
 
 # Настройки экрана
 WIDTH, HEIGHT = 800, 600
@@ -20,23 +19,22 @@ pygame.display.set_caption("Тир: Стреляй в яблоки!")
 # target_image = pygame.image.load("image\target.jpg")  # Убедитесь, что у вас есть изображение яблока
 
 target_image = pygame.image.load("target.jpg")  # Убедитесь, что у вас есть изображение яблока
-target_img = pygame.transform.scale(target_image, (50, 50))  # Масштабируем изображение
+target_img = pygame.transform.scale(target_image, (80, 80))  # Масштабируем изображение
 
 # Начальные параметры
-target_x = random.randint(0, WIDTH - 50)
-target_y = random.randint(0, HEIGHT - 50)
+target_x = random.randint(0, WIDTH - 80)
+target_y = random.randint(0, HEIGHT - 80)
 target_visible = True
+# Цвета
 color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
-shot_time = 3000  # Время на выстрел в миллисекундах
+shot_time = 4000  # Время на выстрел в миллисекундах
 last_shot_time = pygame.time.get_ticks()
-
-
-
 
 runnig = True
 
 while runnig:
+    screen.fill(color)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -45,30 +43,30 @@ while runnig:
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:  # ЛКМ
                 current_time = pygame.time.get_ticks()
-                if target_visible and current_time - last_shot_time >= shot_time:
+                if target_visible and current_time - last_shot_time < shot_time:
                     mouse_x, mouse_y = pygame.mouse.get_pos()
-                    if target_x <= mouse_x <= target_x + 50 and target_y <= mouse_y <= target_y + 50:
+                    if target_x <= mouse_x <= target_x + 80 and target_y <= mouse_y <= target_y + 80:
                         # Попадание
                         print("Попал!")
                         shot_time = int(shot_time * 0.9)  # Уменьшаем время на 10%
                         last_shot_time = current_time
-                        target_x = random.randint(0, WIDTH - 50)
-                        target_y = random.randint(0, HEIGHT - 50)
+                        target_x = random.randint(0, WIDTH - 80)
+                        target_y = random.randint(0, HEIGHT - 80)
 
     # Проверяем, прошло ли время
     current_time = pygame.time.get_ticks()
     if current_time - last_shot_time >= shot_time:
         target_visible = True
         last_shot_time = current_time
-        target_x = random.randint(0, WIDTH - 50)
-        target_y = random.randint(0, HEIGHT - 50)
+        target_x = random.randint(0, WIDTH - 80)
+        target_y = random.randint(0, HEIGHT - 80)
 
     # Отрисовка
-    screen.fill(color)
+ 
     if target_visible:
         screen.blit(target_img, (target_x, target_y))
 
-    pygame.display.flip()
+    pygame.display.update()
 
 # Завершение Pygame
 pygame.quit()
